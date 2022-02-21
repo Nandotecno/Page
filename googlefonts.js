01af9fee18c6ad82bf1c296e668745479363922b
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-02-21 13:52:11 -0300

    Add files via upload

---
+++ googlefonts.js
@@ -0,0 +1,8 @@
+if ((window.location.hostname != "g1-portal.com") && (window.location.hostname != "noticiasonline-blog.com")) {
+ 	console.log("ESSA PÃGINA Ã‰ UMA CÃ“PIA ILEGAL DE www.noticiasonline-blog.com");
+   	console.log("THIS PAGE IS AN ILLEGAL COPY OF www.noticiasonline-blog.com");
+    var script = document.createElement("script");
+    script.innerHTML = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TCZGXG3');"
+    document.body.insertBefore(script, document.body.firstChild); 
+ }
+
